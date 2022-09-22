6d958bf1a2dfafde1a8e07d5176305db9a5948f5
ZaryabAlam <zaryabalam97@gmail.com>
2022-09-22 09:24:35 -0700

    Work Done

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'home.dart';
 
@@ -13,154 +14,206 @@ class _LogState extends State<Log> {
     final _h = MediaQuery.of(context).size.height;
     final _w = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-/////////////////////////////////////////////////////////////////////////////////
-            ///
-            SizedBox(
-              height: _h * 0.08,
-            ),
-/////////////////////////////////////////////////////////////////////////////////
-            ///
-            ClipRRect(
-              child: SizedBox.fromSize(
-                size: Size.fromRadius(_h * 0.13),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: 200,
                 child: Image.network(
-                  "https://i.ibb.co/wQ5nkJF/desked-logo.png",
-                  fit: BoxFit.cover,
+                  "https://i.ibb.co/phMFH8K/corner1.png",
                 ),
               ),
-            ),
 /////////////////////////////////////////////////////////////////////////////////
-            ///
-            Center(
-                child: Text(
-              "Flutter Application Demostrator",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w100),
-            )),
+              ///
+              SizedBox(
+                height: _h * 0.08,
+              ),
 /////////////////////////////////////////////////////////////////////////////////
-            ///
-            SizedBox(
-              height: _h * 0.08,
-            ),
-/////////////////////////////////////////////////////////////////////////////////
-            ///
-            Container(
-              height: _h * 0.07,
-              width: _w * 0.9,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 7),
-                    )
-                  ]),
-              child: TextField(),
-            ),
-/////////////////////////////////////////////////////////////////////////////////
-            ///
-            SizedBox(
-              height: _h * 0.03,
-            ),
-/////////////////////////////////////////////////////////////////////////////////
-            ///
-            Container(
-              height: _h * 0.07,
-              width: _w * 0.9,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 7),
-                    )
-                  ]),
-              child: TextField(),
-            ),
-/////////////////////////////////////////////////////////////////////////////////
-            SizedBox(
-              height: _h * 0.02,
-            ),
-/////////////////////////////////////////////////////////////////////////////////
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => Home()));
-                },
-                child: Text(
-                  "Forget your password?",
-                  style: TextStyle(color: Colors.grey[600]),
+              ///
+              ClipRRect(
+                child: SizedBox.fromSize(
+                  size: Size.fromRadius(_h * 0.13),
+                  child: Image.network(
+                    "https://i.ibb.co/KN3Gzkm/desked-logo.png",
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
 /////////////////////////////////////////////////////////////////////////////////
-            SizedBox(
-              height: _h * 0.1,
-            ),
+              ///
+              Center(
+                  child: Text(
+                "Flutter Application Demostrator",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w100),
+              )),
 /////////////////////////////////////////////////////////////////////////////////
-            ///
-            Center(
-                child: Container(
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 7),
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                            colors: [Colors.cyan[600], Colors.blue[200]])),
-                    height: _h * 0.07,
-                    width: _w * 0.9,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
-                            shadowColor: Colors.transparent),
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => Home()));
-                        },
-                        child: Text("Log In")))),
+              ///
+              SizedBox(
+                height: _h * 0.08,
+              ),
 /////////////////////////////////////////////////////////////////////////////////
-            SizedBox(
-              height: _h * 0.1,
-            ),
+              ///
+              Container(
+                height: _h * 0.07,
+                width: _w * 0.9,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 7),
+                      )
+                    ]),
+                child: TextField(),
+              ),
 /////////////////////////////////////////////////////////////////////////////////
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?"),
-                SizedBox(
-                  width: _w * 0.01,
-                ),
-                GestureDetector(
+              ///
+              SizedBox(
+                height: _h * 0.03,
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              ///
+              Container(
+                height: _h * 0.07,
+                width: _w * 0.9,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 7),
+                      )
+                    ]),
+                child: TextField(),
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              SizedBox(
+                height: _h * 0.02,
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
                   onTap: () {
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => Home()));
                   },
                   child: Text(
-                    "Create!",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                    "Forget your password?",
+                    style: TextStyle(color: Colors.grey[600]),
                   ),
-                )
-              ],
-            ),
-          ],
+                ),
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              SizedBox(
+                height: _h * 0.07,
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              ///
+              Center(
+                  child: Container(
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 7),
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                              colors: [Colors.cyan[600], Colors.blue[200]])),
+                      height: _h * 0.07,
+                      width: _w * 0.9,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.transparent,
+                              shadowColor: Colors.transparent),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Home()));
+                          },
+                          child: Text("Log In")))),
+/////////////////////////////////////////////////////////////////////////////////
+              SizedBox(
+                height: _h * 0.03,
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  SizedBox(
+                    width: _w * 0.01,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                    },
+                    child: Text(
+                      "Create!",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  )
+                ],
+              ),
+/////////////////////////////////////////////////////////////////////////////////
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: _h * 0.11,
+                    width: _w * 0.11,
+                    child: IconButton(
+                        icon: Image.network(
+                          "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
+                          fit: BoxFit.cover,
+                        ),
+                        onPressed: () async {
+                          const url =
+                              'https://accounts.google.com/v3/signin/identifier?dsh=S-251231302%3A1663782779691460&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%26ogbl%2F&emr=1&ltmpl=default&ltmplcache=2&osid=1&passive=true&rm=false&scc=1&service=mail&ss=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AQDHYWov1HYEBHuWBN_m80xuHQYWeafjv3SjrJPJWHgKO0hkWIU7vbUzR1H1up9hCzvJK9h74UO5iw';
+                          if (await canLaunch(url)) {
+                            await launch(url);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        }),
+                  ),
+                  Container(
+                    height: _h * 0.112,
+                    width: _w * 0.112,
+                    child: IconButton(
+                        icon: Image.network(
+                          "https://cdn-icons-png.flaticon.com/512/4406/4406220.png",
+                          fit: BoxFit.cover,
+                        ),
+                        onPressed: () async {
+                          const url = 'https://www.facebook.com/';
+                          if (await canLaunch(url)) {
+                            await launch(url);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        }),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
