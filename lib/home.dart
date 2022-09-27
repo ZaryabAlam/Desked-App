231b15491242d9364fd7248a187fd2cad3b3f6e4
ZaryabAlam <zaryabalam97@gmail.com>
2022-09-27 21:38:28 +0500

    Work Done

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,43 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+      appBar: AppBar(
+        title: Text("Desked"),
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        leading: IconButton(icon: Icon(Icons.menu_rounded), onPressed: () {}),
+        actions: [
+          IconButton(icon: Icon(Icons.swap_vert_rounded), onPressed: () {}),
+          IconButton(icon: Icon(Icons.filter_alt_rounded), onPressed: () {}),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.topRight,
+                  colors: [Colors.cyan[600], Colors.blue[200]])),
+        ),
+      ),
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.grey,
+          elevation: 2,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_rounded, color: Colors.grey),
+              label: 'Add',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.edit, color: Colors.grey),
+              label: 'Edit',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.delete_rounded, color: Colors.grey),
+              label: 'Delete',
+            ),
+          ]),
+/////////////////////////////////////////////////////////////////////////////////////////////////////
       body: Column(
         children: [Text("Desked")],
       ),
