86bf84c4ee64dd5553638116d15568dca66d2c86
ZaryabAlam <zaryabalam97@gmail.com>
2022-09-29 21:14:57 +0500

    Work Done

--- lib/home.dart
+++ lib/home.dart
@@ -7,14 +7,14 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
-  List<dynamic> lst = [""];
+  List<dynamic> lst = ["Flutter Development"];
   var out = "";
 
   Widget build(BuildContext context) {
     final _h = MediaQuery.of(context).size.height;
     final _w = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white10,
 /////////////////////////////////////////////////////////////////////////////////////////////////////
       appBar: AppBar(
         title: Text("Desked"),
@@ -86,89 +86,118 @@ class _HomeState extends State<Home> {
                 height: _h * 0.01,
               ),
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.black26,
-                    borderRadius: BorderRadius.circular(_h * 0.04)),
-                height: _h * 0.20,
-                width: _w * 0.95,
-                child: ListTile(
-                  title: Text("${lst[index]}"),
-                  trailing: Container(
-                    padding: const EdgeInsets.only(top: 10),
-                    width: _w * 0.15,
-                    child: Row(
-                      children: [
+              Column(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(_h * 0.04),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: Offset(0, 7),
+                            )
+                          ]),
+                      height: _h * 0.20,
+                      width: _w * 0.95,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            title: Text("${lst[index]}"),
+                            subtitle: Text("devCat - Zaryab Alam"),
+                            leading: Icon(
+                              Icons.person_pin_rounded,
+                              color: Colors.cyan[600],
+                            ),
+                            trailing: Container(
+                              width: _w * 0.15,
+                              child: Row(
+                                children: [
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-                        GestureDetector(
-                          onTap: () {
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AlertDialog(
-                                    title: TextField(
-                                      onChanged: (value) {
-                                        out = value;
-                                      },
-                                    ),
-                                    actions: [
-                                      ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                            setState(() {
-                                              lst.replaceRange(
-                                                  index, index + 1, {out});
-                                            });
-                                          },
-                                          child: Text('Edit'))
-                                    ],
-                                  );
-                                });
-                          },
-                          child: Icon(Icons.edit),
-                        ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: TextField(
+                                                onChanged: (value) {
+                                                  out = value;
+                                                },
+                                              ),
+                                              actions: [
+                                                ElevatedButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                      setState(() {
+                                                        lst.replaceRange(index,
+                                                            index + 1, {out});
+                                                      });
+                                                    },
+                                                    child: Text('Edit'))
+                                              ],
+                                            );
+                                          });
+                                    },
+                                    child: Icon(Icons.edit),
+                                  ),
+                                  SizedBox(
+                                    width: 0,
+                                  ),
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              lst.removeAt(index);
-                            });
-                          },
-                          child: Icon(Icons.delete),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        lst.removeAt(index);
+                                      });
+                                    },
+                                    child: Icon(Icons.delete),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "The quick fox jumps over a lazy dog The quick fox jumps over a lazy dog The quick fox jumps over a lazy dog",
+                              ),
+                            ),
+                          ),
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+                          SizedBox(
+                            height: _h * 0.02,
+                          ),
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Icon(Icons.location_on, color: Colors.grey),
+                              Text("Karachi"),
+                              Icon(Icons.calendar_today_rounded,
+                                  color: Colors.grey),
+                              Text("30 Sep 2022"),
+                              Icon(Icons.access_time, color: Colors.grey),
+                              Text("11:26"),
+                            ],
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+                          )
+                        ],
+                      )),
+                ],
               )
             ],
           ));
         },
       ),
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: TextField(
-                  onChanged: (value) {
-                    out = value;
-                  },
-                ),
-                actions: [
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        setState(() {
-                          lst.add(out);
-                        });
-                      },
-                      child: Text('Add'))
-                ],
-              );
-            });
-      }),
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
     );
   }
